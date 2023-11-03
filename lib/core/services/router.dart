b20b3fe3842eec3bbdb7c57715d133ac7af00e9c
bijeck <bijecksuzz@gmail.com>
2023-11-03 22:10:29 +0700

    first commit

---
+++ lib/core/services/router.dart
@@ -0,0 +1,33 @@
+import 'package:education_app/core/common/views/page_under_construction.dart';
+import 'package:education_app/on_boarding/presentation/on_boarding_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case OnBoardingScreen.routeName:
+      return _pageBuilder(
+        (_) => const OnBoardingScreen(),
+        settings: settings,
+      );
+    default:
+      return _pageBuilder(
+        (_) => const PageUnderConstruction(),
+        settings: settings,
+      );
+  }
+}
+
+PageRouteBuilder<dynamic> _pageBuilder(
+  Widget Function(BuildContext) page, {
+  required RouteSettings settings,
+}) {
+  return PageRouteBuilder(
+    settings: settings,
+    transitionsBuilder: (_, animation, __, child) => FadeTransition(
+      opacity: animation,
+      child: child,
+    ),
+    pageBuilder: (context, _, __) => page(context),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:education_app/core/res/colours.dart';
+import 'package:education_app/core/res/fonts.dart';
+import 'package:education_app/core/services/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Education App',
+      theme: ThemeData(
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: Fonts.poppins,
+        appBarTheme: const AppBarTheme(
+          color: Colors.transparent,
+        ),
+        colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+      ),
+      onGenerateRoute: generateRoute,
+    );
+  }
+}
