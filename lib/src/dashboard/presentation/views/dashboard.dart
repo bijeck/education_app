10e3f32d3bee894ebf6e5f188227a886895afdb7
bijeck <bijecksuzz@gmail.com>
2023-11-14 22:24:03 +0700

    add navigator

--- lib/src/dashboard/presentation/views/dashboard.dart
+++ lib/src/dashboard/presentation/views/dashboard.dart
@@ -1,22 +1,108 @@
-import 'package:education_app/core/extensions/context_extension.dart';
+import 'package:education_app/core/common/app/providers/user_provider.dart';
+import 'package:education_app/core/res/colours.dart';
+import 'package:education_app/src/auth/data/models/user_model.dart';
+import 'package:education_app/src/dashboard/providers/dashboard_controller.dart';
+import 'package:education_app/src/dashboard/utils/dashboard_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iconly/iconly.dart';
+import 'package:provider/provider.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
   static const String routeName = '/dashboard';
 
   @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Dashboard',
-          style: context.theme.textTheme.bodyMedium!.copyWith(
-            color: Colors.white,
-          ),
-        ),
-      ),
+    return StreamBuilder<LocalUserModel>(
+      stream: DashboardUtils.userDataStream,
+      builder: (_, snapshot) {
+        if (snapshot.hasData && snapshot.data is LocalUserModel) {
+          context.read<UserProvider>().user = snapshot.data;
+        }
+        return Consumer<DashboardController>(
+          builder: (_, controller, __) {
+            return Scaffold(
+              body: IndexedStack(
+                index: controller.currentIndex,
+                children: controller.screens,
+              ),
+            bottomNavigationBar: BottomNavigationBar(
+                currentIndex: controller.currentIndex,
+                showSelectedLabels: false,
+                backgroundColor: Colors.white,
+                elevation: 8,
+                onTap: controller.changeIndex,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 0
+                          ? IconlyBold.home
+                          : IconlyLight.home,
+                      color: controller.currentIndex == 0
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'Home',
+                    backgroundColor: Colors.white,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 1
+                          ? IconlyBold.document
+                          : IconlyLight.document,
+                      color: controller.currentIndex == 1
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'Materials',
+                    backgroundColor: Colors.white,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 2
+                          ? IconlyBold.chat
+                          : IconlyLight.chat,
+                      color: controller.currentIndex == 2
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'Chat',
+                    backgroundColor: Colors.white,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      controller.currentIndex == 3
+                          ? IconlyBold.profile
+                          : IconlyLight.profile,
+                      color: controller.currentIndex == 3
+                          ? Colours.primaryColour
+                          : Colors.grey,
+                    ),
+                    label: 'User',
+                    backgroundColor: Colors.white,
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
---
+++ lib/src/dashboard/utils/dashboard_utils.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:education_app/core/services/injections/injections.dart';
+import 'package:education_app/src/auth/data/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class DashboardUtils {
+  const DashboardUtils._();
+
+  static Stream<LocalUserModel> get userDataStream => getIt<FirebaseFirestore>()
+      .collection('users')
+      .doc(getIt<FirebaseAuth>().currentUser!.uid)
+      .snapshots()
+      .map((event) => LocalUserModel.fromMap(event.data()!));
+}
